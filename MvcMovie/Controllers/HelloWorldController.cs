f5fb63dd14c97b5e3a4724c92dadd94784faaa4e
Sebastián Saavedra <sebastiansaavedr@gmail.com>
2023-01-24 13:24:44 -0300

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        //Get: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        //
+        //Get: /HelloWorld/Welcome/
+        public string Welcome(string name = "redacted", int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hola {name}, el valor de NumTimes es: {numTimes}");
+        }
+    }
+}
